095909fcd09477c1eac3d87766c8a9cc25f4e7b6
nic netzel and yvonna contreras <netzeln@gmail.com>
2016-04-06 15:41:49 -0700

    Change totalCost into a computed property instead of helper

--- app/components/display-total.js
+++ app/components/display-total.js
@@ -2,4 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   shoppingCart: Ember.inject.service(),
+
+  totalCost: Ember.computed('shoppingCart.items.[]', function(){
+    var items = this.get('shoppingCart.items');
+    var total = 0;
+
+    items.forEach(function(item){
+      total = total + item.get('price');
+    });
+
+    if(total){
+      return "Total Cost: $" + total;
+    }
+    else {
+      return "cart is empty, keep shopping";
+    }
+  })
+
 });
--- app/helpers/cart-total.js
+++ app/helpers/cart-total.js
@@ -1,21 +1,21 @@
-import Ember from 'ember';
-
-export function cartTotal(params) {
-  // shoppingCart: Ember.inject.service(),
-
-
-    var items = params[0];
-
-    var total = 0;
-    items.forEach(function(item){
-      total = total + item.get('price');
-    });
-    if( total){
-      return "Total Cost: $" + total;
-    }
-    else {
-      return "cart is empty, keep shopping";
-    }
-  }
-
-export default Ember.Helper.helper(cartTotal);
+// import Ember from 'ember';
+//
+// export function cartTotal(params) {
+//   // shoppingCart: Ember.inject.service(),
+//
+//
+//     var items = params[0];
+//
+//     var total = 0;
+//     items.forEach(function(item){
+//       total = total + item.get('price');
+//     });
+//     if( total){
+//       return "Total Cost: $" + total;
+//     }
+//     else {
+//       return "cart is empty, keep shopping";
+//     }
+//   }
+//
+// export default Ember.Helper.helper(cartTotal);
